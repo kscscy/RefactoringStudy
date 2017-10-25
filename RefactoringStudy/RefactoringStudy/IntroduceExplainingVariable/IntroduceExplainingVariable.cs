1121fb18fa19d0ccb474c5e2ab191c334d233caf
Soochoul Kang <kscscy@gmail.com>
2017-10-25 09:43:33 +0900

    Introduce Explaining Variable, Split Temporary Variable

---
+++ RefactoringStudy/RefactoringStudy/IntroduceExplainingVariable/IntroduceExplainingVariable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 직관적 임시변수 사용 (Introduce Explaining Variable)
+/// 
+/// 사용된 수식이 복잡할 땐 수식의 결과나 수식의 일부분을 용도에 부합하는 직관적 이름의 임시변수에 대입하자
+/// 
+/// 조건문에서 각 조건 절을 가져와서 직관적 이림의 임시변수를 사용해 그 조건의 의미를 설명하려 할 때 많이 사용한다.
+/// 그 외에 긴 알고리즘에서 임시변수를 사용해서 계산의 각 단계를 설명할 수 있을 때에도 사용한다.
+/// => 임시변수를 남용하면 코드를 보는 메소드만 복잡해지고 보는사람이 이해하기 힘들 수도 있다. 
+/// => 그렇다면 메소드추출을 사용하려 노력한다. 임시변수는 하나의 메소드 안에서만 사용한다.
+/// => 지역변수로 인해 메소드추출이 적용하기 힘들 때에는 어쩔 수 없이 직관적 임시변수를 사용한다.
+/// </summary>
+
+namespace RefactoringStudy.IntroduceExplainingVaraiable
+{
+    class IntroduceExplainingVaraiable
+    {
+        // PrintSampleService.cs 
+        // ... 
+        if(refundPrePaidPoint >= 0)
+        {
+            if(orderItem.PayState.Equals("SBPAYMENT") || orderItem.PayState.Equals("TEMPPAYMENT"))
+            {
+                orderItem.PayType = "POINT";
+                orderItem.PayState = "PAYMENT";
+            }
+            orderItem.PrePaidPoint -= refundPrePaidPoint;
+        }
+        //...
+
+        // 임시변수를 사용해보기
+        bool isRefundPrePaidPoint = refundPrePaidPoint >= 0;
+        bool isSbPayment = orderItem.PayState == "SBPAYMENT";
+        bool isTempPayment = orderItem.PayState == "TEMPPAYMENT";
+
+        if (isRefundPrePaidPoint)
+        {
+            if(isSbPayment || isTempPayment)
+            {
+                orderItem.PayType = "POINT";
+                orderItem.PayState = "PAYMENT";
+            }
+            orderItem.PrePaidPoint -= refundPrePaidPoint;
+        }
+
+        // 메소드 추출
+        public bool checkRefundPayState(string orderItem.PayState)
+        {
+            return orderItem.PayState == "SBPAYMENT" || orderItem.PayState == "TEMPPAYMENT";
+        }
+    }
+}
---
+++ RefactoringStudy/RefactoringStudy/SplitTemporaryVariable/SplitTemporaryVariable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 임시변수 분리 (Split Temporary Variable)
+/// 
+/// 루프변수나 값 누적용 임시변수가 아닌 임시변수에 여러번 값이 대입될 땐 각 대입마다 다른 임시변수를 사용하자
+/// 긴 코드의 계산 결과를 나중에 간편히 참조할 수 있게 저장하는 용도로 사용하는 임시변수에는 값이 한 번만 대입되어야한다.
+/// 두 번 이상 대입된다는 건 그 변수가 메소드 안에 여러 용도로 사용된다는 증거이다.
+/// 여러 용도로 사용되는 변수는 각 용도별로 다른 변수를 사용하게 분리해야 한다.
+/// </summary>
+namespace RefactoringStudy.SplitTemporaryVariable
+{
+    class SplitTemporaryVariable
+    {
+        public void PrintItem()
+        {
+            int result = 0;
+            foreach (item in selectList)
+            {
+                if (item.selected)
+                {
+                    result += 1;
+                }
+            }
+
+            Console.WriteLine("totl selected : " + result);
+            result = 0;
+
+            foreach (item in PriceList)
+            {
+                result += item.Price;
+            }
+            Console.WriteLine("totl price : " + result);
+        }
+    }
+}
