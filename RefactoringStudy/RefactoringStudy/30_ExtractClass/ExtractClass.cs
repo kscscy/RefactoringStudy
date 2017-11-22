5193876c321ca9fbaa09cf0c49f41b26322e5dea
Soochoul Kang <kscscy@gmail.com>
2017-11-22 14:54:38 +0900

    Extract Class & Inline Class

---
+++ RefactoringStudy/RefactoringStudy/30_ExtractClass/ExtractClass.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 클래스 추출 (Extract Class)
+/// 
+/// 두 클래스가 처리해야할 기능이 하나의 클래스에 들어있을 땐 
+/// 새 클래스를 만들고 기존 클래스의 관련 필드와 메소드를 새 클래스로 옮기자.
+/// 
+/// 클래스는 시간이 지날수록 방대해지기 마련이다. 그런 클래스에는 많은 메소드와 데이터가 들어있다.
+/// 어느 부분을 분리할지 궁리해서 떼어내야 한다. 함께 변하거나 의존적인 데이터의 일부분도 클래스로 떼어내기 좋다.
+/// 이것을 판단하는 좋은 방법은 데이터나 메소드를 하나 제거하면 어떻게 될지, 다른 필드와 메소드를 추가하는 것은 합리적인지 생각해보라.
+/// 
+/// </summary>
+
+namespace RefactoringStudy._30_ExtractClass
+{
+    // before
+    abstract class Shape
+    {
+        public void Draw()
+        {
+            try
+            {
+                // draw
+            }
+            catch (Exception e)
+            {
+                LogError(e);
+            }
+        }
+
+        public static void LogError(Exception e)
+        {
+            File.WriteAllText(@"c:\Errors\Exception.txt", e.ToString());
+        }
+    }
+
+    // after
+    class Logger
+    {
+        public static void LogError(Exception e)
+        {
+            File.WriteAllText(@"c:\Errors\Exception.txt", e.ToString());
+        }
+    }
+
+    abstract class Shape2
+    {
+        public void Draw()
+        {
+            try
+            {
+                // draw
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e);
+            }
+        }
+    }
+}
---
+++ RefactoringStudy/RefactoringStudy/31_InlineClass/InlineClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 클래스 내용 직접 삽입 (Inline Class)
+/// 
+/// 클래스에 기능이 너무 적을 땐 그 클래스의 모든 기능을 다른 클래스로 합쳐 넣고 원래의 클래스는 삭제하자.
+/// 
+/// ExtractClass와 반대되는 내용이다. 클래스가 더 이상 제 역할을 수행하지 못하여 존재할 이유가 없을 때 실시한다.
+/// 리팩토링을 실시해서 남은 기능이 거의 없어졌을 때 나타난다. 
+/// 이럴 때는 작아진 클래스를 가장 많이 사용하는 다른 클래스를 하나 고른 후 이클래스를 거기에 합쳐야 한다.
+/// </summary>
+namespace RefactoringStudy._31_InlineClass
+{
+    // before
+    class Painter
+    {
+        private Color myColor;
+
+        public Painter(Color c)
+        {
+            myColor = c;
+            InitPainter(myColor);
+        }
+
+        private void InitPainter(Color color)
+        {
+            //init painter
+        }
+    }
+
+    class Circle
+    {
+        private Painter myPainter;
+
+        public Circle(Color c)
+        {
+            myPainter = new Painter(c);
+        }
+    }
+
+    // after
+    class Circle
+    {
+        private Color myColor;
+
+        public Circle(Color c)
+        {
+            myColor = c;
+            InitPainter(myColor);
+        }
+
+        private void InitPainter(Color color)
+        {
+            //init painter
+        }
+    }
+}
