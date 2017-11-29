2f953812959432b2e7f19b725defed717fc13962
Soochoul Kang <kscscy@gmail.com>
2017-11-29 11:59:21 +0900

    Hide Delegate & Remove Middle Man

---
+++ RefactoringStudy/RefactoringStudy/32_HideDelegate/HideDelegate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 대리 객체 은폐 (Hide Delegate)
+/// 
+/// 클라이언트가 객체의 대리 클래스를 호출할 땐, 대리 클래스를 감추는 메소드를 서버에 작성하자
+/// 
+/// 클라이언트가 서버 객체의 필드 중 하나에 정의된 메소드를 호출할 때 그 클라이언트는 이 대리 객체에 관해 알아야 한다.
+/// 대리 객체가 변경될 때 클라이언트도 변경해야 할 가능성이 있기 때문이다.
+/// 의존성을 없애기 위해서 대리 객체를 감추는 위임 메소드를 서버에 두면 된다. 변경은 서버에서만 이뤄지고 클라이언트에는 영향을 주지 않는다.
+/// 
+/// 
+/// 
+/// </summary>
+
+namespace RefactoringStudy._32_HideDelegate
+{
+    // before
+    class Employee
+    {
+        public Department WorkDepartment { get; set; }
+    }
+
+    class Department
+    {
+        public string ChargeCode { get; set; }
+        public Employee Manager { get; set; }
+    }
+
+    class TestEmployee
+    {
+        void Test()
+        {
+            var tester = new Employee();
+            var testManager = tester.WorkDepartment.Manager;
+        }
+    }
+
+    // after
+    class Employee2
+    {
+        private Department2 WorkDepartment { get; set; }
+        public Employee2 Manager
+        {
+            get { return WorkDepartment.Manager; }
+        }
+    }
+
+    class Department2
+    {
+        public string ChargeCode { get; set; }
+        public Employee2 Manager { get; set; }
+    }
+
+    class TestEmployee2
+    {
+        void Test()
+        {
+            var tester = new Employee2();
+            var testManager = tester.Manager;
+        }
+    }
+}
---
+++ RefactoringStudy/RefactoringStudy/33_RemoveMiddleMan/RemoveMiddleMan.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 과잉 중개 메소드 제거 (Remove Middle Man)
+/// 
+/// 클래스에 자잘한 위임이 너무 많을 땐 대리 객체를 클라이언트가 직접 호출하게 하자.
+/// 
+/// Hide Delegate를 사용할 때의 단점은 클라이언트가 대리 개체의 새 기능을 사용해야 할 때마다 서버에 간단한 위임 메소드를 추가해야 한다는 점이다.
+/// 서버 클래스는 그저 중개자에 불과하므로, 이때는 클라이언트가 대리 객체를 직접 호출하게 해야한다.
+/// 
+/// 은폐의 적절한 정도를 알기란 어렵다. 시스템이 변경되면 은폐 정도의 기준도 변하고 필요할 때마다 보수하자.
+/// 
+/// </summary>
+
+namespace RefactoringStudy._33_RemoveMiddleMan
+{
+    // before
+    class Employee
+    {
+        private Department WorkDepartment { get; set; }
+
+        public Employee Manager
+        {
+            get { return WorkDepartment.Manager; }
+        }
+
+        public string ChargeCode
+        {
+            get { return WorkDepartment.ChargeCode; }
+        }
+    }
+
+    class Department
+    {
+        public string ChargeCode { get; set; }
+        public Employee Manager { get; set; }
+    }
+
+    class TestEmployee
+    {
+        void Test()
+        {
+            var tester = new Employee();
+            var testManager = tester.Manager;
+        }
+    }
+
+    // after
+    class Employee2
+    {
+        public Department2 WorkDepartment { get; set; }
+    }
+
+    class Department2
+    {
+        public string ChargeCode { get; set; }
+        public Employee2 Manager { get; set; }
+    }
+
+    class TestEmployee2
+    {
+        void Test()
+        {
+            var tester = new Employee2();
+            var testManager = tester.WorkDepartment.Manager;
+        }
+    }
+}
