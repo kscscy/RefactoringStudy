c0636b80d9f081dc430a6fbdaeec7837bd36db49
Soochoul Kang <kscscy@gmail.com>
2017-12-13 14:06:51 +0900

    self encapsulate field & replace data value with objects

---
+++ RefactoringStudy/RefactoringStudy/36_SelfEncapsulateField/SelfEncapsulateField.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 필드 자체 캡슐화 (Self Encapsulate Field)
+/// 
+/// 필드에 직접 접근하던 중 그 필드로의 결합에 문제가 생길 땐,
+/// 그 필드용 get/set 메소드를 작성해서 두 메소드를 통해서만 필드에 접근하게 만들자.
+/// 
+/// 변수 간접 접근 방식을 사용하면 하위 클래스가 메소드에 해당 정보를 가져오는 방식을 재정의 할 수 있으며, 
+/// 데이터관리가 유연해진다는 장점이 있다.
+/// 변수 직접 접근 방식의 장점은 코드를 알아보기가 쉽다는 것이다.
+/// </summary>
+namespace RefactoringStudy._36_SelfEncapsulateField
+{
+    // before
+    class Range
+    {
+        private int low, high;
+
+        bool Includes(int arg)
+        {
+            return arg >= low && arg <= high;
+        }
+    }
+
+    // after
+    class Range2
+    {
+        private int low, high;
+
+        int Low
+        {
+            get { return low; }
+        }
+        int High
+        {
+            get { return high; }
+        }
+
+        bool Includes(int arg)
+        {
+            return arg >= Low && arg <= High;
+        }
+    }
+}
---
+++ RefactoringStudy/RefactoringStudy/37_ReplaceDataValueWithObject/ReplaceDataValueWithObject.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 데이터 값을 객체로 전환(Replace Data Value with Object)
+/// 
+/// 데이터 항목에 데이터나 기능을 더 추가해야 할 때는 데이터 항목을 객체로 만들자.
+/// Extract Class의 특별한 경우이다. 클래스 내부의 관련성을 증가시킨다. 데이터와 관련된 행동들이 하나의 클래스 안에 들어있다.
+/// 
+/// </summary>
+
+namespace RefactoringStudy._37_ReplaceDataValueWithObject
+{
+    // before
+    class Order
+    {
+        public string Customer { get; set; }
+
+        public Order(string customer)
+        {
+            Customer = customer;
+        }
+
+        private static int NumberOfOrdersFor(IEnumerable<Order> orders, string customer)
+        {
+            return orders.Count(o => o.Customer.Equals(customer, StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+
+    // after
+    class Order2
+    {
+        public Customer Customer { get; set; }
+
+        public Order2(Customer customer)
+        {
+            Customer = customer;
+        }
+
+        private static int NumberOfOrdersFor(IEnumerable<Order2> orders, string customer)
+        {
+            return orders.Count(o => o.Customer.Equals(customer));
+        }
+    }
+
+
+
+    class Customer
+    {
+        public string Name { get; private set; }
+
+        public Customer(string name)
+        {
+            Name = name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Name.Equals(((Customer)obj).Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+
+}
