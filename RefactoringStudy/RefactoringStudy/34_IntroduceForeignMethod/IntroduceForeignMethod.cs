8ad76dc7f073506c4ca2e9aa95e17322a7685b5e
Soochoul Kang <kscscy@gmail.com>
2017-12-13 15:57:55 +0900

    test

---
+++ RefactoringStudy/RefactoringStudy/34_IntroduceForeignMethod/IntroduceForeignMethod.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 외래 클래스에 메소드 추가 (Introduce Foreign Method)
+/// 
+/// 사용 중인 서버 클래스에 메소드를 추가해야 하는데 그 클래스를 수정할 수 없을 땐,
+/// 클라이언트 클래스 안에 서버 클래스의 인스턴스를 첫 번째 인자로 받는 메소드를 작성하자.
+/// 
+/// 기능이 필요하다. 그런데 원본 클래스를 수정할 수 없다면 그 메소드를 클라이언트 클래스 안에 작성해야 한다.
+/// 메소드를 여러번 사용한다면 여기저기 중복 작성할 수도 있다. 
+/// 이때 새로 만들 메소드를 외래 메소드로 만들면 원래 서버 메소드에 있어야할 메소드임을 분명히 나타낼 수 있다.
+/// 
+/// </summary>
+namespace RefactoringStudy._34_IntroduceForeignMethod
+{
+    //before
+    class Report
+    {
+        void sendReport()
+        {
+            DateTime nextDay = previousEnd.AddDays(1);
+        }
+    }
+
+    class Report2
+    {
+        void sendReport()
+        {
+            DateTime nextDay = nextDay(previousEnd);
+        }
+
+        private static DateTime NextDay(DateTime date)
+        {
+            return date.AddDays(1);
+        }
+    }
+}
---
+++ RefactoringStudy/RefactoringStudy/35_IntroduceLocalExtension/IntroduceLocalExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 국소적 상속확장 클래스 사용(Introduce Local Extension)
+/// 
+/// 사용 중인 서버 클래스에 여러 개의 메소드를 추가해야 하는데 클래스를 수정할 수 없을 땐,
+/// 새 클래스를 작성하고 그 안에 필요한 여러 개의 메소드를 작성하자.
+/// 이 상속확장 클래스를 원본 클래스의 하위 클래스나 래퍼 클래스로 만들자.
+/// 
+/// 추가해야 하는 메소드가 한 두개 일때에는 Introduce Foreign Method 기법을 사용하면 된다.
+/// 하지만 필요한 메소드 수가 세 개 이상이면 적당한 곳에 모아둬야 한다.
+/// 
+/// 국소적 상속확장 클래스는 별도의 클래스지만 상속확장하는 클래스의 하위 타입이다. 
+/// 원본 클래스의 모든 기능도 사용하면서 추가 기능도 들어있다. 
+/// 원본 클래스를 사용할 것이 아니라 국소적 상속확장 클래스를 인스턴스화해서 사용하자.
+/// 
+/// 
+/// </summary>
+namespace RefactoringStudy._35_IntroduceLocalExtension
+{
+    class IntroduceLocalExtension
+    {
+    }
+}
