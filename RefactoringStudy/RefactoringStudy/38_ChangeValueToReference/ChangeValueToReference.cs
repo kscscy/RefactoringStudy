51ac29a8b1275f5464b743a26be97e66d9591eeb
Soochoul Kang <kscscy@gmail.com>
2017-12-20 11:45:20 +0900

    ChangeValueToReference & ChangeReferenceToValue

---
+++ RefactoringStudy/RefactoringStudy/38_ChangeValueToReference/ChangeValueToReference.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 값을 참조로 전환 (Change Value to Reference)
+/// 
+/// 클래스에 같은 인스턴스가 많이 들어 있어서 이것들을 하나의 객체로 바꿔야할 땐
+/// 그 객체를 참조객체로 전환하자.
+/// 
+/// 참조객체는 고객이나 계좌 같은 것이다. 각 객체는 현실에서의 한 객체에 대응하므로,
+/// 둘이 같은지 검사할 때는 객체 ID를 사용한다. 두 객체가 같은지 판단해야 하므로
+/// equals나 hashCode(GetHashCode) 메소드를 재정의해야한다.
+/// 
+/// 1. 생성자를 팩토리 메소드로 전환
+/// 2. 참조 객체로의 접근을 담당할 객체를 정한다.
+/// 3. 객체를 미리 생성할지 사용하기 직전에 생성할지 정한다.
+/// 4. 참조 객체를 반환하게 팩토리 메소드를 수정한다.
+/// </summary>
+
+namespace RefactoringStudy._38_ChangeValueToReference
+{
+    //before
+    class Order
+    {
+        private Customer _customer;
+
+        public string CustomerName
+        {
+            get { return _customer.Name; }
+            set { _customer = new Customer(value); }
+        }
+
+        public Order(string customerName)
+        {
+            _customer = new Customer(customerName);
+        }
+
+        private static int NumberOfOrdersFor(IEnumerable<Order> orders, string customer)
+        {
+            return orders.Count(o => o.CustomerName.Equals(customer, StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+
+    class Customer
+    {
+        public string Name { get; private set; }
+
+        public Customer(string name)
+        {
+            Name = name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Name.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+
+    //after
+    class Order2
+    {
+        private Customer2 _customer;
+
+        public string CustomerName
+        {
+            get { return _customer.Name; }
+            set { _customer = Customer2.GetExsitingCustomer(value); }
+        }
+
+        public Order2(string customerName)
+        {
+            _customer = Customer2.GetExsitingCustomer(customerName);
+        }
+
+        private static int NumberOfOrdersFor(IEnumerable<Order> orders, string customer)
+        {
+            return orders.Count(o => o.CustomerName.Equals(customer, StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+
+    class Customer2
+    {
+        private static HashSet<Customer2> ExistingCustomers = new HashSet<Customer2>();
+
+        static Customer2()
+        {
+            ExistingCustomers.Add(new Customer2("adprint"));
+            ExistingCustomers.Add(new Customer2("tqoon"));
+
+        }
+
+        public string Name { get; private set; }
+
+        private Customer2(string name)
+        {
+            Name = name;
+        }
+        
+        public static Customer2 GetExsitingCustomer(string name)
+        {
+            return ExistingCustomers.Single(s => s.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Name.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+
+
+}
---
+++ RefactoringStudy/RefactoringStudy/39_ChangeReferenceToValue/ChangeReferenceToValue.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 참조를 값으로 전환 (Change Reference to Value)
+/// 
+/// 참조 객체가 작고 수정할 수 없고 관리하기 힘들 땐
+/// 그 참조 객체를 값 객체로 만들자.
+/// 
+/// Change Value to Refernce 기법과 마찬가지로, 
+/// 참조 객체와 값 객체 중 무엇을 사용할지 판단하기가 쉽지만은 않다.
+/// 결정하더라도 나중에 바꿔야 할 때가 많다.
+/// 
+/// 참조 객체를 사용한 작업이 복잡해지는 순간이 참조를 값으로 바꿔야 할 시점이다.
+/// 값 객체는 변경할 수 없어야 한다는 주요 특성이 있다.
+/// '변경불가'의 뜻을 명확히 해야한다.
+/// 
+/// ex) Currency(통화)
+/// 1. 전환할 객체가 변경불가인지 변경 가능인지 검사하자.
+/// 2. Equals와 HashCode 메소드를 작성하자.
+/// 3. 팩토리 메소드를 삭제하고, 생성자를 public으로 만들어야 좋을지 생각해보자.
+/// </summary>
+namespace RefactoringStudy._39_ChangeReferenceToValue
+{
+    //before
+    class Currency
+    {
+        public string Name { get; private set; }
+
+        private Currency(string name)
+        {
+            Name = name;
+        }
+
+        public static Currency Create(string name)
+        {
+            return new Currency(name);
+        }
+    }
+
+    //after
+    class Currency2
+    {
+        public string Name { get; private set; }
+
+        public Currency2(string name)
+        {
+            Name = name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Name.Equals(((Currency2)obj).Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+
+    class Test
+    {
+        public bool TestFunc()
+        {
+            return new Currency2("JPY").Equals(new Currency2("JPY"));
+        }
+    }
+}
