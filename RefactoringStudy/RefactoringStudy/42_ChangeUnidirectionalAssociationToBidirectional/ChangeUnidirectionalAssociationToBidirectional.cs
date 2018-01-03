ea0431dde2bad3dd9cde4434fc3107007307bfba
Soochoul Kang <kscscy@gmail.com>
2018-01-03 14:52:09 +0900

    Change Unidirectional Association to Bidirectional & Change Bidirectional Association To Unidirectional

---
+++ RefactoringStudy/RefactoringStudy/42_ChangeUnidirectionalAssociationToBidirectional/ChangeUnidirectionalAssociationToBidirectional.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 클래스의 단방향 연결을 양방향으로 전환 (Change Unidirectional Association to Bidirectional)
+/// 
+/// 두 클래스가 서로의 기능을 사용해야 하는데 한 방향으로만 연결되어 있을 땐
+/// 역 포인터(?)를 추가하고 두 클래스를 모두 업데이트할 수 있게 접근 한정자를 수정하자.
+/// 
+/// 한 클래스에서 다른 클래스를 참조하게 해 놓고 나중에 역으로 참조해야 할 경우가 생긴다면?
+/// 포인터를 역방향으로 참조해야 한다.
+/// 포인터는 단방향 연결이라서 이런 식의 역방향 참조는 불가능하다. 
+/// 다른 경로를 찾아서 해결할 수 있을 때도 있다.
+/// 그렇게 하면 계산에 비용이 들긴 해도 합리적이며, 이 기능을 사용하는 메소드를 참조되는 클래스에 넣을 수 있다.
+/// 
+/// 하지만 이게 쉽지 않을 떄도 있으므로 양방향 참조(역포인터)를 설정해야 한다.
+/// 
+/// - 양방향 연결은 단방향 연결보다 구현 및 유지관리가 훨씬 어렵다.
+/// - 양방향 연결은 클래스를 상호 의존적으로 만든다. 단방향 연결을 사용하면 그 중 하나를 다른 연결과 독립적으로 사용할 수 있다.
+/// 
+/// 
+/// </summary>
+
+namespace RefactoringStudy._42_ChangeUnidirectionalAssociationToBidirectional
+{
+    // before
+    class Order
+    {
+        public Customer Customer { get; set; }
+    }
+
+    class Customer
+    {
+        private HashSet<Order> _orders = new HashSet<Order>();
+
+        public IEnumerable<Order> Orders
+        {
+            get { return _orders; }
+        }
+        
+        internal HashSet<Order> FriendOrders
+        {
+            get { return _orders; }
+        }
+    }
+
+    // after
+    class Order2
+    {
+        private HashSet<Customer2> _customers2 = new HashSet<Customer2>();
+        public IEnumerable<Customer2> Customers2
+        {
+            get { return _customers2; }
+        }
+
+        public void AddCustomer(Customer2 customer2)
+        {
+            customer2.FriendOrders.Add(this);
+            _customers2.Add(customer2);
+        }
+
+        public void RemoveCustomer(Customer2 cusomter2)
+        {
+            cusomter2.FriendOrders.Remove(this);
+            _customers2.Remove(cusomter2);
+        }
+
+        //private Customer2 _customer2;
+        //public Customer2 Customer2
+        //{
+        //    get { return _customer2; }
+        //    set
+        //    {
+        //        _customer2.FriendOrders.Remove(this);
+        //        _customer2 = value;
+        //        _customer2.FriendOrders.Add(this);
+        //    }
+        //}
+    }
+
+    class Customer2
+    {
+        private HashSet<Order2> _orders = new HashSet<Order2>();
+
+        public IEnumerable<Order2> Orders
+        {
+            get { return _orders; }
+        }
+
+        internal HashSet<Order2> FriendOrders
+        {
+            get { return _orders; }
+        }
+
+        public void AddOrder(Order2 order2)
+        {
+            order2.AddCustomer(this);
+        }
+
+        public void RemoveOrder(Order2 order2)
+        {
+            order2.RemoveCustomer(this);
+        }
+    }
+}
---
+++ RefactoringStudy/RefactoringStudy/43_ChangeBidirectionalAssociationToUnidirectional/ChangeBidirectionalAssociationToUnidirectional.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 클래스의 양방향 연결을 단방향으로 전환 (Change Bidirectional Association To Unidirectional)
+/// 
+/// 두 클래스가 양방향으로 연결되어 있는데 한 클래스가 다른 클래스의 기능을 더 이상 사용하지 않게 됐을 땐
+/// 불필요한 방향의 연결을 끊자.
+/// 
+/// 양방향 연결은 쓸모가 많지만 대가가 따른다.
+/// 양방향 연결을 유지하고 객체가 적절히 생성되고 제거되는지 확인하는 복잡함이 더해진다.
+/// 
+/// 양방향 연결이 많으면 좀비 객체가 발생하기도 쉽다. 좀비 객체란 참조가 삭제 되지 않아 제거돼야 함에도
+/// 남아서 떠도는 객체를 뜻한다.
+/// 
+/// 양방향 연결로 인해 두 클래스는 서로 종속된다. 한 클래스를 수정하면 다른 클래스도 변경된다.
+/// 종속성이 많으면 시스템의 결합력이 강해져서 사소한 수정에도 예기치 못한 각종 문제가 발생한다.
+/// 그러므로 양방향 연결은 꼭 필요할 때만 사용해야 한다.
+/// 양방향 연결이 더 이상 쓸모없다고 판단되면 불필요한 연결을 차단하자.
+/// 
+/// </summary>
+
+namespace RefactoringStudy._43_ChangeBidirectionalAssociationToUnidirectional
+{
+    // before
+    class Order
+    {
+        private Customer _customer;
+        public Customer Customer
+        {
+            get { return _customer; }
+            set
+            {
+                _customer.FriendOrders.Remove(this);
+                _customer = value;
+                _customer.FriendOrders.Add(this);
+            }
+        }
+    }
+
+    class Customer
+    {
+        private HashSet<Order> _orders = new HashSet<Order>();
+
+        public IEnumerable<Order> Orders
+        {
+            get { return _orders; }
+        }
+
+        internal HashSet<Order> FriendOrders
+        {
+            get { return _orders; }
+        }
+    }
+
+    // after
+    class Order2
+    {
+        public Customer2 Customer2
+        {
+            get
+            {
+                var customers = Customer2.FindCustomers(new CustomerSearch { Order2 = this });
+                if (customers.Count() < 1)
+                    throw new Exception("Not Found");
+                if (customers.Count() > 1)
+                    throw new Exception("Multiple Customers found for Order");
+                return customers.Single();
+            }
+        }
+    }
+
+    class Customer2
+    {
+        private HashSet<Order2> _orders2 = new HashSet<Order2>();
+
+        public HashSet<Order2> Orders2
+        {
+            get { return _orders2; }
+        }
+
+        public static IEnumerable<Customer2> GetAllCustomers()
+        {
+            return new List<Customer2>();
+        }
+
+        public static IEnumerable<Customer2> FindCustomers(CustomerSearch customerSearch)
+        {
+            return new List<Customer2>();
+        }
+    }
+
+    class CustomerSearch
+    {
+        public Order2 Order2 { get; set; }
+    }
+}
