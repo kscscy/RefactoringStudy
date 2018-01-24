b4c3892820b9db466f67352999e9cb744cc02c5f
Soochoul Kang <kscscy@gmail.com>
2018-01-24 14:36:06 +0900

    Replace Magic Number With Symbolic Constant & Encapsulate Field

---
+++ RefactoringStudy/RefactoringStudy/44_ReplaceMagicNumberWithSymbolicConstant/ReplaceMagicNumberWithSymbolicConstant.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 마법 숫자를 기호 상수로 전환 (Replace Magic Number With Symbolic Constant)
+/// 
+/// 특수 의미를 지닌 리터럴 숫자가 있을 땐 
+/// 의미를 살린 이름의 상수를 작성한 후 리터럴 숫자를 그 상수로 교체하자.
+/// 
+/// 여러곳에서 논리적으로 같은 숫자를 참조해야 할 때 특히 문제가 많다.
+/// 숫자가 변경될 가능성이 있다면 끔찍한 일이다.
+/// 변경하지 않더라도 코드에서 어떻게 처리되고 있는지 알아내기 힘들다.
+/// 
+/// 숫자가 분류 부호라면 부호를 클래스로 전환 기법을 적용하고
+/// 숫자가 배열의 길이라면 length를 사용해서 루프를 실행하자
+/// </summary>
+namespace RefactoringStudy._44_ReplaceMagicNumberWithSymbolicConstant
+{
+    class ReplaceMagicNumberWithSymbolicConstant
+    {
+        // before
+        double getCircleArea(int radius)
+        {
+            return radius * radius * 3.14;
+        }
+
+        // after
+        public const double PI = 3.14;
+
+        double getCircleArea2(int radius)
+        {
+            // Math.PI
+            return Math.Pow(radius, radius) * PI;
+        }
+    }
+}
---
+++ RefactoringStudy/RefactoringStudy/45_EncapsulateField/EncapsulateField.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 필드 캡슐화(Encapsulate Field)
+/// 
+/// public 필드가 있을 땐
+/// 그 필드를 private로 만들고 필드용 읽기, 쓰기 메소드를 작성하자
+/// 
+/// 객체지향의 주요 원칙 중 하나는 캡슐화다. 캡슐화를 데이터은닉 이라고 부르기도 한다.
+/// 데이터는 절대로 public 타입으로 선언하면 안된다.
+/// 데이터가 있는 객체가 모르는 사이에 다른 객체가 데이터값을 읽고 변경할 수 있다.
+/// 
+/// 필드 캡슐화의 첫 단계는 데이터를 은닉하고 읽기/쓰기 메소드를 추가하는 것이다.
+/// 읽기/쓰기 메소드만 있는 클래스는 객체의 장점을 활용하지 않은 클래스이다.
+/// 필드 캡슐화를 적용한 후에는 새 메소드를 사용하는 메소드를 찾아 그 메소드를 묶어서 메소드 이동을 적용해준다.
+/// 
+/// </summary>
+
+
+namespace RefactoringStudy._45_EncapsulateField
+{
+    class EncapsulateField
+    {
+        // before
+        public string Name { get; set; }
+
+        // after
+        private string _name;
+        public string Name2
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+    }
+}
