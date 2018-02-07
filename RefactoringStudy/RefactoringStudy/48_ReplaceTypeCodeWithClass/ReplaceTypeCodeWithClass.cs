05d4632fd8f71f9ef5b39bed32b95ef03239b58f
Soochoul Kang <kscscy@gmail.com>
2018-02-07 15:18:00 +0900

    Replace Type Code With Class & Replace Type Code With Subclasses

---
+++ RefactoringStudy/RefactoringStudy/48_ReplaceTypeCodeWithClass/ReplaceTypeCodeWithClass.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 분류 부호를 클래스로 전환 (Replace Type Code With Class)
+/// 
+/// 기능에 영향을 미치는 숫자형 분류 부호가 든 클래스가 있을 땐 그 숫자를 새 클래스로 바꾸자
+/// 
+/// 분류 부호 이름을 상징적인 것으로 정하면 코드가 상당히 이해하기 쉬워진다.
+/// 문제는 상징적 이름은 단지 별명에 불과하다는 점이다.
+/// 컴파일러는 이름이 아닌 숫자를 보고 판단하기 때문에 별명에 불과하다.
+/// 분류 부호를 파라미터로 받는 메소드는 숫자만을 인자로 받으며 이름을 전달할 수는 없다.
+/// 그래서 코드를 이해하기 힘들어질 수 있다.
+/// 
+/// 숫자형 분류 부호를 클래스로 빼내면 컴파일러는 그 클래스 안에서 종류 판단을 수행할 수 있다.
+/// 클래스 안에 팩토리 메소드를 작성하면 유효한 인스턴스만 생성되는 지와 그런 인스턴스가 적절한 객체로
+/// 전달되는지를 정적으로 검사할 수 있다.
+/// 
+/// 분류부호를 클래스로 만드는 건 분류 부호가 순수한 데이터일 때만 실시해야 한다.
+/// 분류부호 switch 문 안에 사용되어 다른 기능을 수행하거나 메소드를 호출할 땐 클래스로 전환하면 안된다.
+/// switch문에는 임의로 클래스를 사용할 수 없으며 오직 정수 타입만을 사용하자.
+/// 
+/// </summary>
+
+namespace RefactoringStudy._48_ReplaceTypeCodeWithClass
+{
+    // before
+    class Person
+    {
+        public static int O = 0;
+        public static int A = 1;
+        public static int B = 2;
+        public static int AB = 3;
+
+        public int BloodGroup { get; private set; }
+
+        public Person(int bloodGroup)
+        {
+            BloodGroup = bloodGroup;
+        }
+    }
+
+    // after
+    class Person2
+    {
+        public BloodGroup BloodGroup { get; private set; }
+
+        public Person2(BloodGroup bloodGroup)
+        {
+            BloodGroup = bloodGroup;
+        }
+    }
+
+    class BloodGroup
+    {
+        public static BloodGroup O = new BloodGroup(0);
+        public static BloodGroup A = new BloodGroup(1);
+        public static BloodGroup B = new BloodGroup(2);
+        public static BloodGroup AB = new BloodGroup(3);
+        
+        private int Code { get;  set; }
+
+        private BloodGroup(int code)
+        {
+            Code = code;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Code == ((BloodGroup)obj).Code;
+        }
+
+        public override int GetHashCode()
+        {
+            return Code.GetHashCode();
+        }
+    }
+}
---
+++ RefactoringStudy/RefactoringStudy/49_ReplaceTypeCodeWithSubclasses/ReplaceTypeCodeWithSubClasses.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 분류 부호를 하위 클래스로 전환 (Replace Type Code With Subclasses)
+/// 
+/// 클래스 기능에 영향을 주는 변경불가 분류 부호가 있을 땐 분류 부호를 하위 클래스로 만들자
+/// 
+/// 분류 부호가 클래스 기능에 영향을 미치는 현상은 case 문 같은 조건문이 있을 때 주로 나타난다.
+/// 그런 조건문은 swich문 아니면 if-else문이다.
+/// 어느 조건문이든 분류 부호의 값을 검사해서 그 값에 따라 다른 코드를 실행한다.
+/// 
+/// 이런 조건문은 조건문을 재정의로 전환을 실시해서 재정의로 바꿔야 한다.
+/// 이 기법이 효과를 보려면 분류 부호를 다형화된 기능이 든 상속 구조로 고쳐야 한다.
+/// 그런 상속 구조는 하나의 클래스와 각 분류 부호에 대한 하위클래스로 구성된다.
+/// 
+/// 분류부호가 든 클래스 코드를 이용해 각 분류 부호별 하위클래스를 작성하자. 
+/// 이 기법을 적용할 수 없는 몇 가지 경우는 다음과 같다.
+/// 
+/// - 분류 부호의 값이 객체 생성 후 변할 때
+/// - 다른 이유로 분류 부호를 이미 하위클래스로 만들었을 때
+/// - 분류 부호를 상태/전략 패턴으로 전환을 실시해야 할 때
+/// 
+/// 
+/// </summary>
+
+namespace RefactoringStudy._49_ReplaceTypeCodeWithSubclasses
+{
+    class Employee
+    {
+        public const int ENGINEER = 0;
+        public const int SALESMAN = 1;
+        public const int MANAGER = 2;
+
+        public int Type { get; private set; }
+
+        public Employee(int type)
+        {
+            Type = type;
+        }
+    }
+
+    // after
+    abstract class Employee2
+    {
+        public const int ENGINEER = 0;
+        public const int SALESMAN = 1;
+        public const int MANAGER = 2;
+
+        public abstract int Type { get; /*private set;*/ }
+
+        //private Employee2(int type)
+        //{
+        //    Type = type;
+        //}
+
+        //protected Employee2() { }
+
+        public static Employee2 Create(int type)
+        {
+            //if (type == ENGINEER) return new Engineer();
+            switch (type)
+            {
+                case ENGINEER:
+                    return new Engineer();
+                case SALESMAN:
+                    return new Salesman();
+                case MANAGER:
+                    return new Manager();
+            }
+            //return new Employee2(type);
+            throw new Exception("Unknown Type");
+        }
+    }
+
+    class Engineer : Employee2
+    {
+        public override /*new*/ int Type
+        {
+            get { return Employee2.ENGINEER; }
+        }
+    }
+    class Salesman : Employee2
+    {
+        public override int Type
+        {
+            get { return Employee2.SALESMAN; }
+        }
+    }
+    class Manager : Employee2
+    {
+        public override int Type
+        {
+            get { return Employee2.MANAGER; }
+        }
+    }
+}
