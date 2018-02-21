ad75fca9d475353cf2b802da61b3083aec23fe0b
Soochoul Kang <kscscy@gmail.com>
2018-02-21 11:13:04 +0900

    Replace Type Code With State OR Strategy & Replace Subclass With Fields

---
+++ RefactoringStudy/RefactoringStudy/50_ReplaceTypeCodeWithStateORStrategy/ReplaceTypeCodeWithStateORStrategy.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 분류 부호를 상태/전략 패턴으로 전환 (Replace Type Code With State OR Strategy)
+/// 
+/// 분류 부호가 클래스의 기능에 영향을 주지만 하위 클래스로 전환할 수 없을 땐
+/// 그 분류 부호를 상태 객체로 만들자
+/// 
+/// 이 기법은 분류 부호를 하위클래스로 전환과 비슷하지만
+/// 분류 부호가 객체 수명주기 동안 변할 때나 다른 이유로 하위클래스로 만들 수 없을 때 사용한다.
+/// 
+/// 이 기법은 상태 패턴이나 전략 패턴중 하나를 사용한다.
+/// 이 기법을 사용하면 많은 불필요한 추가 클래스가 생긴다.?
+/// 
+/// 상태 패턴과 전략 패턴을 공부하자.
+/// 하나의 알고리즘을 단순화해야 할 때는 전략이 더 적절하며,
+/// 상태별 데이터를 이동하고 객체를 변화하는 상태로 생각할 때는 상태 패턴이 더 적절하다.
+/// 
+/// 
+/// </summary>
+
+namespace RefactoringStudy._50_ReplaceTypeCodeWithStateORStrategy
+{
+    //
+    class Employee
+    {
+        public const int ENGINEER = 0;
+        public const int SALESMAN = 1;
+        public const int MANAGER = 2;
+
+        public int Type { get; set; }
+
+        public int Salary { get; private set; }
+        public int Comission { get; private set; }
+        public int Bonus { get; private set; }
+
+        public Employee(int type)
+        {
+            Type = type;
+        }
+
+        public int GetPayAmount()
+        {
+            switch(Type)
+            {
+                case ENGINEER:
+                    return Salary;
+                case SALESMAN:
+                    return Salary + Comission;
+                case MANAGER:
+                    return Salary + Bonus;
+            }
+            throw new Exception("Unknown Type");
+        }
+    }
+
+    //after
+    class Employee2
+    {
+        //5. 정의 복사
+        //public const int ENGINEER = 0;
+        //public const int SALESMAN = 1;
+        //public const int MANAGER = 2;
+
+        //4. get set 수정 
+        //private EmployeeType _employeeType;
+        //public int Type
+        //{
+        //    //4.
+        //    //get;
+        //    //set;
+        //    get { return _employeeType.Type; }
+        //    set { _employeeType = EmployeeType.Create(value); }
+        //}
+
+        // 7. 분류부호 정의 삭제 후 클래스 참조로 변환
+        private EmployeeType Type { get; set; }
+
+
+        public int Salary { get; private set; }
+        public int Comission { get; private set; }
+        public int Bonus { get; private set; }
+
+        public Employee2(/*int 7. */ EmployeeType type)
+        {
+            Type = type;
+        }
+
+        //8. 조건문을 재정의로 전환
+        //public int GetPayAmount()
+        //{
+        //    // 7.
+        //    //switch (Type)
+        //    //switch (Type.Type)
+        //    //{
+        //    //    //6. 정의를 복사 후 수정
+        //    //    //case ENGINEER:
+        //    //    case EmployeeType.ENGINEER:
+        //    //        return Salary;
+        //    //    case EmployeeType.SALESMAN:
+        //    //        return Salary + Comission;
+        //    //    case EmployeeType.MANAGER:
+        //    //        return Salary + Bonus;
+        //    //}
+        //    //throw new Exception("Unknown Type");
+        //    return Type.GetPayAmount(this);
+        //}
+    }
+
+
+    // 1. 상태 클래스 선언하기
+    abstract class EmployeeType
+    {
+        // 5. 분류 부호 정의를 복사 > EmployeeType에 대한 팩토리 메소드 작성 후, Employee 클래스의 set 수정
+        public const int ENGINEER = 0;
+        public const int SALESMAN = 1;
+        public const int MANAGER = 2;
+
+        public abstract int Type { get; }
+        
+        // 3. switch 문 생성
+        public static EmployeeType Create(int type)
+        {
+            switch (type)
+            {
+                //case Employee2.ENGINEER:
+                //6. 정의를 복사 후 수정
+                case ENGINEER:
+                    return new Engineer();
+                case SALESMAN:
+                    return new SalesMan();
+                case MANAGER:
+                    return new Manager();
+            }
+            throw new Exception("Unknown Type");
+        }
+
+        //8.
+        public abstract int GetPayAmount(Employee2 employee2);
+        
+    }
+
+
+    // 2. 분류 부호별 하위클래스 작성하기
+
+    class Engineer : EmployeeType
+    {
+        public override int Type
+        {
+            get
+            {
+                //6. 정의를 복사 후 수정
+                //return Employee2.ENGINEER;
+                return ENGINEER;
+            }
+        }
+
+        public override int GetPayAmount(Employee2 employee2)
+        {
+            return employee2.Salary;
+        }
+    }
+
+    class SalesMan : EmployeeType
+    {
+        public override int Type
+        {
+            get
+            {
+                 return SALESMAN;
+            }
+        }
+
+        public override int GetPayAmount(Employee2 employee2)
+        {
+            return employee2.Salary + employee2.Comission;
+        }
+    }
+
+    class Manager : EmployeeType
+    {
+        public override int Type
+        {
+            get
+            {
+                return MANAGER;
+            }
+        }
+
+        public override int GetPayAmount(Employee2 employee2)
+        {
+            return employee2.Salary + employee2.Bonus;
+        }
+    }
+}
---
+++ RefactoringStudy/RefactoringStudy/51_ReplaceSubclassWithFields/ReplaceSubclassWithFields.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 하위클래스를 필드로 전환 (Replace Subclass With Fields)
+/// 
+/// 여러 하위클래스가 상수 데이터를 반환하는 메소드만 다룰 땐
+/// 각 하위클래스의 메소드를 상위클래스 필드로 전환하고 하위클래스는 전부 삭제하자.
+/// 
+/// 기능을 추가하거나 기능을 조금씩 달리할 하위클래스를 작성하자.
+/// 다형적인 기능의 한 형태는 상수메소드다. 상수메소드는 하드코딩된 값을 반환하는 메소드다.
+/// 상수 메소드는 읽기 메소드에 각기 다른 값을 반환하는 하위클래스에 넣으면 유용하다.
+/// 상위 클래스 안에 읽기 메소드를 정의하고 그 읽기 메소드를 하위 클래스에서 다양한 값으로 구현하자.
+/// 
+/// 상수 메소드가 유용하긴 해도, 하위클래스를 상수 메소드로만 구성한다고 해서 효용성이 커지는 것은 아니다.
+/// 상위클래스 안에 필드를 넣고 그런 하위클래스는 완전히 삭제하면 된다.
+/// 그렇게 하면 하위클래스의 불필요한 복잡함도 사라진다.
+/// 
+/// </summary>
+
+namespace RefactoringStudy._51_ReplaceSubclassWithFields
+{
+    //
+    abstract class Person
+    {
+        public abstract bool IsMale { get; }
+        public abstract char Code { get; }
+    }
+
+    class Male : Person
+    {
+        public override bool IsMale
+        {
+            get
+            {
+                return true;
+            }
+        }
+        public override char Code
+        {
+            get
+            {
+                return 'M';
+            }
+        }
+    }
+
+    class Female : Person
+    {
+        public override bool IsMale
+        {
+            get
+            {
+                return false;
+            }
+        }
+        public override char Code
+        {
+            get
+            {
+                return 'F';
+            }
+        }
+    }
+
+    //after 
+    // 6. abstract 제거
+    /*abstract*/ class Person2
+    {
+        // 2. 상위클래스에 상수메소드별 필드 선언
+        // 8. get set 수정
+        //private bool _isMale;
+        //private char _code;
+
+        // 5. 상위클래스에 읽기 메소드를 넣어 필드를 사용하게 만들고 하위클래스의 메소드는 삭제
+        //public abstract bool IsMale { get; }
+        //public abstract char Code { get; }
+        // 8. get set 수정
+        public bool IsMale { get; private set; }
+        public char Code { get; private set; }
+
+       
+
+        // 3. 상위클래스에 protected 타입 생성자 메소드 추가
+        protected Person2(bool isMale, char code)
+        {
+            IsMale = isMale;
+            Code = code;
+        }
+
+        // 1. 생성자를 팩토리 메소드로 전환
+        public static Person2 CreateMale()
+        {
+            //6. 하위 클래스의 내용을 모두 삭제했으면 메소드 내용 직접삽입을 실시해서
+            // 하위클래스 생성자 내용을 상위클래스 안에 직접 넣기
+            //return new Male2();
+            return new Person2(true, 'M');
+        }
+
+        public static Person2 CreateFemale()
+        {
+            return new Person2(false, 'F');
+        }
+    }
+
+    //7. 하위클래스 삭제
+    //class Male2 : Person2
+    //{
+    //    //5. 상위클래스에 읽기 메소드를 넣어 필드를 사용하게 만들고 하위클래스의 메소드는 삭제
+    //    //public override bool IsMale
+    //    //{
+    //    //    get
+    //    //    {
+    //    //        return true;
+    //    //    }
+    //    //}
+    //    //public override char Code
+    //    //{
+    //    //    get
+    //    //    {
+    //    //        return 'M';
+    //    //    }
+    //    //}
+
+    //    // 4. 새로 작성한 생성자를 호출하는 생성자 메소드 추가
+    //    public Male2() : base(true, 'M') { }
+    //}
+
+    //7. 하위클래스 삭제
+    //class Female2 : Person2
+    //{
+    //    //5. 상위클래스에 읽기 메소드를 넣어 필드를 사용하게 만들고 하위클래스의 메소드는 삭제
+    //    //public override bool IsMale
+    //    //{
+    //    //    get
+    //    //    {
+    //    //        return false;
+    //    //    }
+    //    //}
+    //    //public override char Code
+    //    //{
+    //    //    get
+    //    //    {
+    //    //        return 'F';
+    //    //    }
+    //    //}
+
+    //    // 4. 새로 작성한 생성자를 호출하는 생성자 메소드 추가
+    //    public Female2() : base(false, 'F') { }
+    //}
+}
