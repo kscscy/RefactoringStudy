45cf76ae077d47551b9d34c5a4e50538e382f344
Soochoul Kang <kscscy@gmail.com>
2018-05-16 09:13:50 +0900

    Consolidate Duplicate Conditional Fragments & Remove Control Flag

---
+++ RefactoringStudy/RefactoringStudy/54_ConsolidateDuplicateConditionalFragments/ConsolidateDuplicateConditionalFragments.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 조건문의 공통 실행 코드 빼내기 (Consolidate Duplicate Conditional Fragments)
+/// 
+/// 조건문의 모든 절에 같은 실행 코드가 있을 땐 같은 부분을 조건문 밖으로 빼자
+/// 
+/// 각 절이 공통적으로 실행할 기능과 서로 다르게 실행할 기능을 한눈에 알 수 있다.
+/// 공통 코드가 조건문의 앞 절에 있을 땐 조건문 앞으로 빼자.
+/// 공통 코드가 조건문의 끝 절에 있을 땐 조건문 뒤로 빼자.
+/// 공통 코드 명령이 둘 이상일 땐 메소드로 만들자.
+/// 
+/// </summary>
+
+namespace RefactoringStudy._54_ConsolidateDuplicateConditionalFragments
+{
+    class ConsolidateDuplicateConditionalFragments
+    {
+        private bool isSpecialDeal;
+        public double total { get; set; }
+        public int price { get; set; }
+        public void Send()
+        {
+
+        }
+
+        //before
+        void Test()
+        {
+            if (isSpecialDeal)
+            {
+                total = price * 0.95;
+                Send();
+            }
+            else
+            {
+                total = price * 0.98;
+                Send();
+            }
+        }
+
+        //after
+        void Test2()
+        {
+            if (isSpecialDeal)
+                total = price * 0.95;
+            else
+                total = price * 0.98;
+            Send();
+        }
+
+        // Exception 처리에도 이 방식을 적용할 수 있다.
+        // try - catch 구간 안의 예외 발생 명령 뒤에 공통 코드가 들어 있으면  그 코드를 finally 구간으로 옮기면 된다.
+    }
+}
---
+++ RefactoringStudy/RefactoringStudy/55_RemoveControlFlag/RemoveControlFlag.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 제어 플래그 제거 (Remove Control Flag)
+/// 
+/// 논리 연산식의 제어 플래그 역할을 하는 변수가 있을 땐 그 변수를 break 문이나 return문으로 바꾸자
+/// 
+/// 논리문을 빠져나오게 하는 제어 플래그 값을 찾자.
+/// 제어 플래그 값을 대입하는 코드를 break문이나 continue문으로 바꾸자
+/// 
+/// break 문이나 continue 문이 없는 언어에서는 다음과 같이 하자
+/// - 로직을 메소드로 빼자.
+/// - 논리문을 빠져나오게 하는 제어 플래그 값을 찾자.
+/// - 빠져나오게 하는 값을 return 문으로 바꾸자.
+/// 
+/// </summary>
+
+namespace RefactoringStudy._55_RemoveControlFlag
+{
+    class RemoveControlFlag
+    {
+        void SendAlert()
+        {
+
+        }
+
+        //before
+        void Test1(IEnumerable<string> people)
+        {
+            var foundPerson = false;
+            foreach (var person in people)
+            {
+                if (!foundPerson)
+                {
+                    if ("Don" == person)
+                    {
+                        SendAlert();
+                        foundPerson = true;
+                    }
+                    if ("John" == person)
+                    {
+                        SendAlert();
+                        foundPerson = true;
+                    }
+                }
+            }
+        }
+
+        //after - 제어 플래그를 break문으로
+        void Test1_1(IEnumerable<string> people)
+        {
+            // 2. 제어 플래그 제거
+            //var foundPerson = false;
+            foreach (var person in people)
+            {
+                // 2.
+                //if (!foundPerson)
+
+                if ("Don" == person)
+                {
+                    SendAlert();
+                    //foundPerson = true;
+                    break; // 1. break
+                }
+                if ("John" == person)
+                {
+                    SendAlert();
+                    //foundPerson = true;
+                    break;
+                }
+
+            }
+        }
+
+        //before
+        void Test2(IEnumerable<string> people)
+        {
+            var foundPerson = string.Empty;
+            foreach (var person in people)
+            {
+                if (foundPerson == "")
+                {
+                    if ("Don" == person)
+                    {
+                        SendAlert();
+                        foundPerson = person;
+                    }
+                    if ("John" == person)
+                    {
+                        SendAlert();
+                        foundPerson = person;
+                    }
+                }
+            }
+            Console.WriteLine(foundPerson);
+        }
+
+        //after - 제어 플래그를 reteurn문으로
+        void Test2_1(IEnumerable<string> people)
+        {
+            //string foundPerson = NewMethod(people);
+            //Console.WriteLine(foundPerson);
+            Console.WriteLine(NewMethod(people));
+        }
+
+        // 1. 메소드 추출
+        private string NewMethod(IEnumerable<string> people)
+        {
+            // 3.제어 플래그 제거
+            //var foundPerson = string.Empty;
+            foreach (var person in people)
+            {
+                //if (foundPerson == "")
+                if ("Don" == person)
+                {
+                    SendAlert();
+                    //foundPerson = person;
+                    return person; // 2. return문으로 교체
+                }
+                if ("John" == person)
+                {
+                    SendAlert();
+                    //foundPerson = person;
+                    return person;
+                }
+            }
+
+            return string.Empty; //3.
+        }
+    }
+}
